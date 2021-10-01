ef1971cfa44a185dfad563dcb3f5864980bef016
Ben H <deepfreespace@gmail.com>
2021-09-30 23:11:05 -0600

    Added JS comments

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -12,39 +12,49 @@
 7. generatePassword() creates password and returns to user by setting text field on page to generated password */
 
 // BEGIN Assignments
+// Getters
 var generateBtn = document.querySelector("#generate");
 var submitCriteriaBtn = document.querySelector("#submit-criteria");
 var resetCriteriaBtn = document.querySelector("#reset-criteria");
 var formEl = document.querySelector("#criteria-form");
-var lowercaseChars = [];
-var uppercaseChars = [];
-var numericChars = [];
-var specialChars = [];
+// Variable assignments/declarations
+var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericChars = "0123456789";
+var specialChars = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 var userCriteria = {};
 // END Assignments
 
 // BEGIN Function Declarations
 // Function to get user's criteria, validate it, and update criteria object if valid
 function getCriteria() {
+  // Get the user's desired length and, just in case it's a float, round it to the nearest integer
   var userLength = Math.round(parseFloat(document.getElementById("password-length").value));
+  // Assume user's desired length is invalid until tested below
   var userLengthValid = false;
+  // Determine which boxes the user checked
   var userLC = document.getElementById("lowercase-chars").checked;
   var userUC = document.getElementById("uppercase-chars").checked;
   var userNC = document.getElementById("numerical-chars").checked;
   var userSC = document.getElementById("special-chars").checked;
+  // Check if user's desired length is within acceptable parameters
   if (userLength > 7 && userLength < 129) {
     userLengthValid = true;
   }
 
+  // If user's length is invalid AND they haven't selected any characters
   if (!userLengthValid && !userLC && !userUC && !userNC && !userSC) {
     alert("You must enter a valid password length AND select at least one character type to generate your password.");
     return false;
+  // If user has entered valid length but not selected any characters
   } else if (userLengthValid && !userLC && !userUC && !userNC && !userSC) {
     alert("You must select at least one character type to generate your password.");
     return false;
+  // If user's length is invalid but they have selected characters
   } else if (!userLengthValid) {
     alert("You must enter a valid password length to generate your password.");
     return false;
+  // Push the criteria to the criteria object
   } else {
     userCriteria.passwordLength = userLength;
     userCriteria.lowercaseSelected = userLC;
@@ -52,13 +62,20 @@ function getCriteria() {
     userCriteria.numericalSelected = userNC;
     userCriteria.specialSelected = userSC;
   }
+  
   alert("Your criteria have been saved!");
-  // formEl.reset();
 }
 
 // Function to reset criteria fields
 function resetCriteria() {
   formEl.reset();
+}
+
+// Function to generate password
+function generatePassword() {
+  // Create a temporary array that will be used to generate a password
+  tempArray = [];
+
 }
 
 // Write password to the password display field
