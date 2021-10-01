f7d7b9d0c003ecf644406b317ee159b015c7dfbc
Ben H <deepfreespace@gmail.com>
2021-10-01 10:32:57 -0600

    Removed pseudocode

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,16 +1,3 @@
-/* PSEUDOCODE
-1. User clicks button, triggering writePassword()
-2. writePassword() calls getCriteria()
-3. getCriteria() prompts user for the following criteria
-  a. Password length (between at 8 and 128 characters, inclusive)
-  b. Included character types (lowercase, uppercase, numeric, special)
-    i. Special characters are:  !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
-4. Each prompt validates responses; if no valid response given, user is asked to try again
-  a. At least one character type must be selected
-5. Each prompt response stored as values in object (probably as true/false)
-6. getCriteria() returns object, passes to generatePassword()
-7. generatePassword() creates password and returns to user by setting text field on page to generated password */
-
 // BEGIN Assignments
 // Getters
 var generateBtn = document.querySelector("#generate");
